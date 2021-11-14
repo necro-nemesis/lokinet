44d452268c1d29ed1dc2050cc63147099a83a718
necro-nemesis <necro_nemesis@hotmail.com>
2021-11-13 19:12:52 -0500

    use full path to ipk

--- contrib/openwrt/ci-upload.sh
+++ contrib/openwrt/ci-upload.sh
@@ -29,7 +29,7 @@ br="${br//\//-}"
 upload_to="builds.lokinet.dev/${DRONE_REPO// /_}/$br/$base"
 
 put=
-for ipk in openwrt/${apkmarch}/*.ipk; do
+for ipk in /drone/src/openwrt/${apkmarch}/*.ipk; do
     put+=$'\n'"put $ipk $upload_to"
 
     echo -e "\n\n\e[35;1m$apkarch contents:\e[0m"
